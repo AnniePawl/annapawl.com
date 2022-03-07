ba7a8e491cbf6e2527c30d8e4dcd06ad59fff0ac
Anna Pawl <annampawl@gmail.com>
2022-03-07 14:09:49 -0500

    radio1

--- src/components/shelves/Shelves.js
+++ src/components/shelves/Shelves.js
@@ -3,6 +3,7 @@ import Shelf1 from "./Shelf1";
 import Shelf2 from "./Shelf2";
 import Shelf3 from "./Shelf2";
 import Books from "../books/Books";
+import Radio from "../radio/radio";
 import "./shelves.scss";
 
 function Shelves() {
@@ -13,7 +14,11 @@ function Shelves() {
         <Shelf1 />
       </div>
 
-      <Shelf2 />
+      <div className="shelf-2">
+        <Radio />
+        <Shelf2 />
+      </div>
+
       <Shelf3 />
     </div>
   );
